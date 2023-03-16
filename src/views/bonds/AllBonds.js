32577892d91967db238d78f68d4a00c669bf7c14
urandu <bildadnamawa@gmail.com>
2023-03-16 21:32:29 +0300

    Bonds page

--- src/views/bonds/AllBonds.js
+++ src/views/bonds/AllBonds.js
@@ -1,7 +1,32 @@
+import { cilPlus } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
+import { CCard, CCardBody, CCardHeader, CCol, CRow, CButton, CCardTitle } from '@coreui/react'
 import React from 'react'
+import BondsTable from './BondsTable'
 
 const Bonds = () => {
-  return <></>
+  return (
+    <>
+      <CRow>
+        <CCol xs={12}>
+          <CCard>
+            <CCardHeader>
+              <span className="float-end">
+                <CButton size="sm" color="success">
+                  <CIcon icon={cilPlus} size="sm" />
+                  Add new bond
+                </CButton>
+              </span>
+            </CCardHeader>
+            <CCardBody>
+              {/* <CCardTitle>Bonds</CCardTitle> */}
+              <BondsTable></BondsTable>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  )
 }
 
 export default Bonds
