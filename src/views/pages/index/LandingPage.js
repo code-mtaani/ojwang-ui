e85f74a11cc9595635f7fc6e00af43429fde34d8
BildadOwuor <bildadowuor@gmail.com>
2023-04-08 01:35:19 +0300

    updates and fixes

--- src/views/pages/index/LandingPage.js
+++ src/views/pages/index/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCol,
@@ -9,6 +10,11 @@ import {
   CDropdownToggle,
   CForm,
   CFormInput,
+  CCard,
+  CCardBody,
+  CCardImage,
+  CCardTitle,
+  CCardText,
   CNavbar,
   CNavbarBrand,
   CNavbarNav,
@@ -21,25 +27,30 @@ import BlogPost from './sections/BlogSection'
 
 const LandingPage = () => {
   const [searchQuery, setSearchQuery] = useState('')
+const navigate = useNavigate()
+
   const handleSearchSubmit = (e) => {
     e.preventDefault()
     //set searchQuery to input text
   }
 
+  const handleLoginClick = () => {
+    navigate('/login')
+  }
   return (
     <>
       <CNavbar color="light" light expand="md" className="px-3">
         <CContainer fluid>
-          <CNavbarBrand>Ojwang</CNavbarBrand>
+          <CNavbarBrand><h3 style={{color:"green"}}>Ojwang</h3></CNavbarBrand>
           <CNavbarNav>
             <CNavItem>
-              <CNavLink href="#">Home</CNavLink>
+              <CNavLink href="/index" style={{fontWeight:"bold", fontSize:"24px"}}>Home</CNavLink>
             </CNavItem>
             <CNavItem>
-              <CNavLink href="#">Features</CNavLink>
+              <CNavLink href="#" style={{fontWeight:"bold", fontSize:"24px"}}>Features</CNavLink>
             </CNavItem>
             <CNavItem>
-              <CNavLink href="#">Developers</CNavLink>
+              <CNavLink href="#" style={{fontWeight:"bold", fontSize:"24px"}}>Developers</CNavLink>
             </CNavItem>
           </CNavbarNav>
           <CForm className="d-flex" onSubmit={handleSearchSubmit}>
@@ -50,67 +61,89 @@ const LandingPage = () => {
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
             />
-            <CButton type="submit" color="primary">
+            <CButton type="submit" color="primary" style={{}}>
               Search
             </CButton>
           </CForm>
           <CDropdown>
             <CDropdownToggle caret>Get Started</CDropdownToggle>
             <CDropdownMenu>
-              <CDropdownItem>Individual</CDropdownItem>
-              <CDropdownItem>Business</CDropdownItem>
+              <CDropdownItem onClick={handleLoginClick}>Login</CDropdownItem>
+              <CDropdownItem>Sign Up</CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
         </CContainer>
       </CNavbar>
       <CContainer fluid className="px-3">
         <CRow>
-          <CCol>
-            <h1>Hi there</h1>
-            <p>
+          <CCol md={4}>
+            <h1 style={{fontSize: '28px', fontWeight:'bold' }}>Hi there</h1>
+            <p style={{fontSize:'18px'}}>
               Ojwang is a platform that lists all government bonds that are on sale, their portfolio and coupon payment dates.
-
             </p>
-            <CButton color="primary">Learn more</CButton>
+            <CButton color="primary"> <h5 style={{color:'#ccc'}}>Learn more</h5></CButton>
           </CCol>
-
+          <CCol md={4} className="ms-auto">
+            {/* <CImage align="end" src="https://images.pexels.com/photos/210600/pexels-photo-210600.jpeg" /> */}
+          </CCol>
         </CRow>
       </CContainer>
       <CContainer className="py-5">
-      <hr style={{borderTop: '1px solid #ccc', with: '20px'}} />
-      <h2 className="mb-4 text-center" style={{fontSize: '28px', fontWeight: 'bold'}}>Features</h2>
+      <hr style={{borderTop: '1px solid #ccc', with: '20px' }} />
+      <h2 className="mb-4 text-center" style={{fontSize: '30px', color:'green', fontWeight: 'bold'}}>Features</h2>
       <CRow>
-        <CCol md={4} className="mb-4">
-          <h5 className="text-center mb-3" style={{fontSize: '24px'}}>List of Government Bonds</h5>
-          <p className="text-center" style={{fontSize: '18px'}}>View all government bonds that are currently available for sale. Stay updated with the latest bond offerings and make informed investment decisions.</p>
+      <CCol md={4} className="mb-4">
+          <CCard style={{ width: '24rem' }}>
+              <CCardImage orientation="top" src={'https://media.business-review.eu/unsafe/420x250/smart/filters:contrast(5):quality(80)/business-review.eu/wp-content/uploads/2018/04/bonds.jpg'} width={200} height={ 300 } />
+            <CCardBody>
+                <CCardTitle style={{fontWeight:'bold', fontSize:'20px'}}> List of Government Bonds</CCardTitle>
+              <CCardText>
+                View all government bonds that are currently for sale. Stay updated with the latest bond offerings and make informed investment decisions.
+              </CCardText>
+            </CCardBody>
+          </CCard>
         </CCol>
         <CCol md={4} className="mb-4">
-          <h5 className="text-center mb-3" style={{fontSize: '24px'}}>Coupon Payment Dates</h5>
-          <p className="text-center" style={{fontSize: '18px'}}>Know the dates when you can expect coupon payments for the bonds in your portfolio. Plan your finances effectively and stay on top of your bond investments.</p>
+          <CCard style={{ width: '24rem' }}>
+            <CCardImage orientation="top" src={'https://www.incharge.org/wp-content/uploads/2020/07/Bond-Indices.jpg'} width={ 200 } height={ 300 }  />
+            <CCardBody>
+              <CCardTitle style={{fontWeight:'bold', fontSize:'20px'}}>Coupon Payment Dates</CCardTitle>
+              <CCardText>
+              Know the dates when you can expect coupon payments for the bonds in your portfolio. Plan your finances effectively and stay on top of your bond investments.
+              </CCardText>
+            </CCardBody>
+          </CCard>
         </CCol>
-        <CCol md={4} className="mb-4">
-          <h5 className="text-center mb-3" style={{fontSize: '24px'}}>Detailed Analytics</h5>
-          <p className="text-center" style={{fontSize: '18px'}}>Get a comprehensive analysis of your bond portfolio. Track your investment performance, view historical data, and gain insights to make informed investment strategies.</p>
+          <CCol md={4} className="mb-4">
+          <CCard style={{ width: '24rem' }}>
+            <CCardImage orientation="top" src={'https://www.howardcountymd.gov/sites/default/files/styles/image_560_x_408_/public/2021-04/Bonds.gif?itok=oU5KQd3j'} width={ 200 } height={ 300 }  />
+            <CCardBody>
+              <CCardTitle style={{fontWeight:'bold', fontSize:'20px'}}>Detailed Analytics</CCardTitle>
+              <CCardText>
+              Get a comprehensive analysis of your bond portfolio. Track your investment performance, view historical data, and gain insights to make informed investment strategies.
+              </CCardText>
+            </CCardBody>
+          </CCard>
         </CCol>
-            </CRow>
-            <hr style={{borderTop: '1px solid #ccc', with: '20px'}} />
-            </CContainer>
-          <CContainer fluid>
+      </CRow>
+      <hr style={{borderTop: '1px solid #ccc', with: '20px'}} />
+      </CContainer>
+        <CContainer fluid>
           <h2 className="mt-5 mb-4 text-center" style={{fontSize: '28px', fontWeight: 'bold'}}>Latest Blog Posts</h2>
         <BlogPost />
         </CContainer>
-      <CContainer fluid className="py-5 bg-primary text-white text-center">
-      <h2 className="mb-4">Ready to Get Started?</h2>
-      <p className="mb-4">
-        Sign up for an account today.
-      </p>
-      <CButton color="light" className="mb-3">Sign Up Now</CButton>
-      <br />
-      <small>Or <a href="#">learn more</a> about Ojwang</small>
-    </CContainer>
+        <CContainer fluid className="py-5 bg-primary text-white text-center">
+        <h2 className="mb-4">Ready to Get Started?</h2>
+        <p className="mb-4">
+          Sign up for an account today.
+        </p>
+        <CButton color="light" className="mb-3">Sign Up Now</CButton>
+        <br />
+        <small>Or <a href="#">learn more</a> about Ojwang</small>
+        </CContainer>
       <AppFooter/>
     </>
-  );
-};
+  )
+}
 
 export default LandingPage
